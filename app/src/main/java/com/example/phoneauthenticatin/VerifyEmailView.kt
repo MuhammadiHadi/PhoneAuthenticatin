324dba95105dce5d9f8797914b9f93c7e5f037b6
HYPER <meerhadikhan364@gmail.com>
2023-03-10 15:43:33 +0500

    email verifycation done

--- app/src/main/java/com/example/phoneauthenticatin/VerifyEmailView.kt
+++ app/src/main/java/com/example/phoneauthenticatin/VerifyEmailView.kt
@@ -6,15 +6,12 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
 import com.example.phoneauthenticatin.databinding.VerifyEmailLayoutBinding
 import com.example.phoneauthenticatin.utils.MainUtils
-import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.actionCodeSettings
-import java.util.*
 
 
 class VerifyEmailView : AppCompatActivity() {
@@ -30,44 +27,61 @@ class VerifyEmailView : AppCompatActivity() {
 
 
         val khan=MainUtils()
-        binding.verifyEmailLink.setOnClickListener{
+
+        binding.verifyEmailLink.setOnClickListener {
 
 
-           val emailID=binding.emailLink.text.toString()
-            binding.progressBar3.visibility= View.VISIBLE
+            val emailID = binding.emailLink.getText().toString()
+            binding.progressBar3.visibility = View.VISIBLE
 
-            val actionCodeSettings = ActionCodeSettings.newBuilder()
-                .setUrl("https://phone-auth-f0bfe.web.app")
-                .setHandleCodeInApp(true)
-                .setAndroidPackageName(
-                    "com.example.phoneauthenticatin",
-                    false, /* installIfNotAvailable */
-                    "12" /* minimumVersion */)
-                .build()
-          if(emailID.isNotEmpty()){
-              auth.sendSignInLinkToEmail(emailID, actionCodeSettings)
-                  .addOnCompleteListener(OnCompleteListener<Void?> { task ->
-                      Log.d(TAG, "onComplete: ")
-                      if (task.isSuccessful) {
 
-                          khan.showToast(this,"please check your Email")
-                          binding.progressBar3.visibility= View.INVISIBLE
 
-                      } else {
 
-                          println("userEmail$emailID ID and actioncode$actionCodeSettings")
-                          khan.showToast(this,"Error")
-                          binding.progressBar3.visibility= View.INVISIBLE
-                      }
-                  })
-          }else{
-              binding.progressBar3.visibility= View.INVISIBLE
-              khan.showToast(this,"Enter Email")
-          }
+                if (emailID.isNotEmpty()) {
+                    val actionCodeSettings = actionCodeSettings {
+                        // URL you want to redirect back to. The domain (www.example.com) for this
+                        // URL must be whitelisted in the Firebase Console.
+                        url = ("https://phone-auth.com")
+                        // This must be true
+                        handleCodeInApp = true
+                        setAndroidPackageName(
+                            BuildConfig.APPLICATION_ID,
+                            true, /* installIfNotAvailable */
+                            "12" )/* minimumVersion */
+
+                    }
+                    try{
+                        auth.sendSignInLinkToEmail(emailID, actionCodeSettings)
+                        .addOnCompleteListener { task ->
+                            Log.d(TAG, "onComplete: ")
+                            if (task.isSuccessful) {
+
+                                khan.showToast(this, "please check your Email")
+                                binding.progressBar3.visibility = View.INVISIBLE
+
+                            } else {
+
+                                println("userEmail$emailID ID and actioncode+> $actionCodeSettings")
+                                khan.showToast(this, "Error")
+                                binding.progressBar3.visibility = View.INVISIBLE
+                            }
+                        }
+
+                    } catch (e: FirebaseException) {
+                        // Handle errors
+
+
+                        println("Hello firbase$e.toString()")
+                    }
+                } else {
+                    binding.progressBar3.visibility = View.INVISIBLE
+                    khan.showToast(this, "Enter Email")
+                }
+
 
         }
 
 
-
     }
+
 }
